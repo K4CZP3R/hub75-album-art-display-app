9c2c7efcd18f668170e899ce3c8815794d2f0509
K4CZP3R <kacper@kacperserewis.net>
2023-08-20 03:14:08 +0200

    already rotated :D

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -12,7 +12,6 @@ export default async function handler(
 ) {
   const spot = new SpotifyWebApi({
     clientId: "868eeab41f0246c0bfb3cbcddaf8fa55",
-    clientSecret: "d5d8822abb6442a3b879f1cc8900b444",
     redirectUri: "http://localhost:3000/api/callback",
   });
 
